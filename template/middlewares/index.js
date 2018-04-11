da0368cd9b3d8813a32a7746030e77fef1f692cb
wangweihua <aztack@163.com>
2018-04-11 17:07:05 +0800

    fix:load middleware with glob.sync not glob.glob

--- template/middlewares/index.js
+++ template/middlewares/index.js
@@ -4,14 +4,8 @@ require('colors')
 
 module.exports = function (app) {
   // load middlewares
-  glob.glob($path.join(__dirname, './*.middleware.js'), function (err, files) {
-    if (err) {
-      console.error('Load middleware failed!'.red.bold)
-    } else {
-      files.forEach(function (file) {
-        console.log(('Loading middleware from ' + file).green.bold)
-        app.use(require(file))
-      })
-    }
+  glob.sync($path.join(__dirname, './*.middleware.js')).forEach(function (file) {
+    console.log(('Loading middleware from ' + file).green.bold)
+    app.use(require(file))
   })
 }
